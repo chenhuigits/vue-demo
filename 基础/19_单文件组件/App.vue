735845215b4a7f09b0049759e7ef5e2be4cbefb7
chenhuigits <ch_java@163.com>
2023-05-20 10:58:07 +0800

    【练习vue】非单文件组件 单文件组件

---
+++ 基础/19_单文件组件/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>{{ msg }}</h2>
+        <School></School>
+    </div>
+</template>
+<script>
+    import School from './School.vue';
+    export default{
+        name:"App",
+        data(){
+            return{
+                msg:"欢迎来到尚硅谷学习！"
+            }
+        },
+        components:{
+            School
+        }
+    }
+</script>
