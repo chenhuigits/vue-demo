fbdc88cb3831b04aad71104eb7b9a150de11ca68
chenhuigits <ch_java@163.com>
2023-05-21 13:41:04 +0800

    【练习Vue】vue2.0 脚手架

---
+++ vue_test/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+ <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <School></School>
+  </div>
+</template>
+
+<script>
+//引入组件
+import School from './components/School.vue'
+export default {
+  name: 'App',
+  components: {
+    School
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_test/src/components/School.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="demo">
+        <h2>学校名称：{{ name }}</h2>
+        <h2>学校地址：{{ address }}</h2>
+        <Student></Student>
+        <button @click="showName">点我提示学校名称</button>
+    </div>
+</template>
+
+<script>
+    import Student from './Student.vue';
+    export default{
+    name: 'School',
+    data() {
+        return {
+            name: "尚硅谷",
+            address: "北京昌平"
+        };
+    },
+    methods: {
+        showName() {
+            alert(this.name);
+        }
+    },
+    components: { Student }
+}
+</script>
+
+<style>
+    .demo{
+        background-color: orange;
+    }
+</style>
